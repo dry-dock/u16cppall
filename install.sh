91e55d1ac3af6859feb9b636e4068275eccf8666
raksha78 <rakshitha27@gmail.com>
2017-07-12 16:55:02 +0530

    update cpp from 6 to 7.1 and clang from 3.9 to 4.0

--- install.sh
+++ install.sh
@@ -8,20 +8,20 @@ apt-get clean
 apt-get update
 echo "======================== Cleaned apt-get ======================"
 
-echo "======================= Installing gcc 6 ======================"
+echo "======================= Installing gcc 7 ======================"
 add-apt-repository ppa:ubuntu-toolchain-r/test
 apt-get update
-apt-get install gcc-6 g++-6
-update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 50 --slave /usr/bin/g++ g++ /usr/bin/g++-6
-update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-6 50
+apt-get install gcc-7 g++-7
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 50 --slave /usr/bin/g++ g++ /usr/bin/g++-7
+update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-7 50
 gcc --version
-echo "================== Successfully Installed gcc 6 ==============="
+echo "================== Successfully Installed gcc 7 ==============="
 
-echo "==================== Installing clang 3.9.0 ==================="
-wget -nv http://llvm.org/releases/3.9.0/clang+llvm-3.9.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
-tar xf clang+llvm-3.9.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
-cd clang+llvm-3.9.0-x86_64-linux-gnu-ubuntu-16.04
+echo "==================== Installing clang 4.0.0 ==================="
+wget -nv http://llvm.org/releases/4.0.0/clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
+tar xf clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
+cd clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-16.04
 cp -R * /usr/local/
 cd ../
 clang --version
-echo "=============== Successfully Installed clang 3.9.0 ============"
+echo "=============== Successfully Installed clang 4.0.0 ============"
