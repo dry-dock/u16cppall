8700022f9ca286d55d310cf4a9d3654891f62faa
bindushree16 <37651858+bindushree16@users.noreply.github.com>
2018-04-02 15:07:11 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,18 +8,18 @@ apt-get clean
 apt-get update
 echo "======================== Cleaned apt-get ======================"
 
-echo "======================= Installing gcc 7.3 ======================"
+echo "======================= Installing gcc 7.2 ======================"
 add-apt-repository ppa:ubuntu-toolchain-r/test
 apt-get update
 apt-get install -y \
-  gcc-7=7.3.0-1ubuntu1~16.04 \
-  g++-7=7.3.0-1ubuntu1~16.04
+  gcc-7=7.2.0-1ubuntu1~16.04 \
+  g++-7=7.2.0-1ubuntu1~16.04
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 50 --slave /usr/bin/g++ g++ /usr/bin/g++-7
 update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-7 50
 gcc --version
-echo "================== Successfully Installed gcc 7.3 ==============="
+echo "================== Successfully Installed gcc 7.2 ==============="
 
-apt-get install autoconf=2.69* automake=1:1.15* ccache=3.4.2* libssl-dev=1.0.2g*
+apt-get install autoconf=2.69* automake=1:1.15* ccache=3.2.4* libssl-dev=1.0.2g*
 
 CLANG_VERSION=5.0.1
 echo "==================== Installing clang $CLANG_VERSION ==================="
